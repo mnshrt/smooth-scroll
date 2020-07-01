c68bf264bc178aa5d3cf31371413414cf8e0fd05
mnshrt <mnshrt2292@gmail.com>
2020-07-01 05:30:46 +0530

    basic date-list made, with focus on today's date on page load

--- src/App.js
+++ src/App.js
@@ -1,121 +1,52 @@
 import React from 'react';
+
 import './App.css';
-import Item from './components/item.component';
-import getList from './data/utility';
+import { deltaDate, getDateArray } from './data/utility';
 
-//Action Types
-const types = {
-  start: "START",
-  loaded: "LOADED"
-};
-
-//Reducer to update the state based on action
-const reducer = (state, action) => {
-  switch (action.type) {
-    case types.start:
-      return { ...state, loading: true };
-    case types.loaded:
-      return {
-        ...state,
-        loading: false,
-        data: [...action.newData],
-      };
-    default:
-      throw new Error("Don't understand action");
-  }
-};
-
-// defining the context to make the state available to the child elements
-export const MyContext = React.createContext();
-
-
-function MyProvider({ children }) {
-  const [state, dispatch] = React.useReducer(reducer, {
-    loading: false,
-    data: []
-  });
-  const { loading, data } = state;
-
-  // updating the list on scrolldown
-  const load = () => {
-    dispatch({ type: types.start });
-
-    setTimeout(() => {
-      const newData = getList(new Date());
-      dispatch({ type: types.loaded, newData });
-    }, 300);
-  };
-
-  return (
-    <MyContext.Provider value={{ loading, data, load }}>
-      {children}
-    </MyContext.Provider>
-  );
-}
-
-
-function App() {
-
-  const { data, loading, load } = React.useContext(MyContext);
-  const loader = React.useRef(load);
-  const observer = React.useRef(
-    new IntersectionObserver(
-      entries => {
-        const first = entries[0];
-        if (first.isIntersecting) {
-          loader.current();
-        }
-      },
-      { threshold: 1 }
-    )
-  );
-  const [element, setElement] = React.useState(null);
-
-  React.useEffect(() => {
-    loader.current = load;
-  }, [load]);
-
-  // update the component on scroll down
-  React.useEffect(() => {
-    const currentElement = element;
-    const currentObserver = observer.current;
-
-    if (currentElement) {
-      currentObserver.observe(currentElement);
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state={
+      dates:[],
+      todaysDate:''
     }
-
-    return () => {
-      if (currentElement) {
-        currentObserver.unobserve(currentElement);
+    this.myRef = React.createRef();
+  }
+  handleScrollToElement(event) {
+    window.scrollTo({
+      top:this.myRef.offsetTop, 
+     behavior: "smooth" 
+  }); 
+  }
+  componentDidMount(){
+      let todaysDate= new Date();
+      let topDate= deltaDate(todaysDate,0,-1,0);
+      let bottomDate=  deltaDate(todaysDate,0,2,0);
+      let data = getDateArray(topDate,bottomDate);
+   
+      this.setState({
+        todaysDate,
+        dates:data
+      })
+      if(this.myRef.current){
+        this.handleScrollToElement();
       }
-    };
-  }, [element]);
+  }
 
-
-  return (
-    <div className="App">
-    <ul>
-      {[...data.entries()].map(entry => (
-        <Item key={entry[0]} itemData={entry[1]}/>
-      ))}
-
-      {loading && <li style={{ background: "transparent" }}>Loading...</li>}
-
-    
-      {
-      //flag for updating the list. once it is fully loaded it triggers the loading action  
-      !loading && (
-        <li ref={setElement} style={{ background: "transparent" }}></li>
-      )}
-    </ul>
-  </div>
-  );
+  render(){
+    const {dates, todaysDate}= this.state
+    return (
+      <div className='app'> 
+        <ul style={{listStyle:"none"}}>{
+          dates.map((date,index)=>(
+          (todaysDate.toString().substring(0,15)===date.toString().substring(0,15))?(
+          <li key={index} ref={this.myref}>  {">>>>>>>"+ date.toString()}</li>):(
+             <li key={index}> {date.toString()}</li>
+          )
+            ))}
+        </ul>
+      </div>
+      );
+    }
 }
-
-export default ()=>{
-    return  (
-        <MyProvider>
-          <App/>
-        </MyProvider>
-    );
-};
+export default App;
--- src/data/utility.js
+++ src/data/utility.js
@@ -1,6 +1,7 @@
 import getData from 'time-interval-lipsum';
 
-function getList(currentTime){
+
+export const getList = (currentTime)=>{
     console.log('hello');
     const start = new Date('2020-01-01T00:00:00');
 
@@ -10,4 +11,24 @@ function getList(currentTime){
 
    
 }
-export default getList;
+export const getDateArray = (start, end) => {
+    let arr = [];
+    let dt = new Date(start);
+    while (dt <= end) {
+        arr.push((new Date(dt))); //save only the Day MMM DD YYYY part
+        dt.setDate(dt.getDate() + 1);
+    }
+    return arr;
+}
+
+export const deltaDate= (input,days, months, years)=>{
+    return new Date(
+        input.getFullYear()+ years,
+        input.getMonth()+months,
+        Math.min(input.getDate()+days,
+        new Date( input.getFullYear()+ years,
+        input.getMonth()+months+1,
+        0).getDate()
+        )
+    )
+}
