86a24918fc89a1ae3660ec50ea32403ef695179a
mnshrt <mnshrt2292@gmail.com>
2020-07-02 06:08:12 +0530

    displaying list with data for a limited range

--- src/App.js
+++ src/App.js
@@ -1,50 +1,92 @@
 import React from 'react';
 
 import './App.css';
-import { deltaDate, getDateArray } from './data/utility';
+import { deltaDate, getDateArray,getList } from './data/utility';
+import Item from './components/item.component';
+
 
 class App extends React.Component {
   constructor(props){
     super(props);
     this.state={
       dates:[],
-      todaysDate:''
+      data:new Map(),
+      infoMap: new Map(),
+      todaysDate:'',
+      loading:true
     }
-    this.myRef = React.createRef();
+  
   }
-  handleScrollToElement(event) {
-    window.scrollTo({
-      top:this.myRef.offsetTop, 
-     behavior: "smooth" 
-  }); 
+
+  getInfo =  (date, newData)=>{
+    let info =  [...newData.entries()].filter(entry=>
+      entry[0].toString().substring(0,15)===date.toString().substring(0,15)
+      )
+   //console.log(info)
+    return info;
+  }
+
+
+  updateInfoMap=  (newData)=>{
+    let infoMap = new Map();
+    let {dates}=  this.state;
+
+    dates.map( date=>{
+      let info =  this.getInfo(date,newData);
+      infoMap.set(date,info||null)
+      return date;
+    })
+    return infoMap;
+  }
+
+  updateTheDataList= async (start,end)=>{  
+    let newData =await getList(start,end);
+    let infoMap= this.updateInfoMap(newData)
+    this.setState({
+      data:newData,
+      infoMap,
+      loading:false
+    });   
   }
   componentDidMount(){
       let todaysDate= new Date();
       let topDate= deltaDate(todaysDate,0,-1,0);
       let bottomDate=  deltaDate(todaysDate,0,2,0);
-      let data = getDateArray(topDate,bottomDate);
-   
+      let datesArray = getDateArray(topDate,bottomDate);
+
+       this.updateTheDataList(topDate, bottomDate);
+     // console.log(this.state.data)
       this.setState({
         todaysDate,
-        dates:data
-      })
-      if(this.myRef.current){
-        this.handleScrollToElement();
-      }
+        dates:datesArray
+      });
+
+  }
+  findDateInfo = async (date)=>{
+    let {data} = this.state;
+    if(data){
+     let dateInfo =  await [...data.entries()].filter(entry=> (entry[0].toString().substring(0,15)===date.toString().substring(0,15)))
+   // console.log(dateInfo)
+    return dateInfo.length===1?dateInfo:null;
+    }
+    return null;
   }
 
   render(){
-    const {dates, todaysDate}= this.state
+    const {loading, dates,data,todaysDate, infoMap}= this.state
+  // console.log(infoMap)
     return (
       <div className='app'> 
+      {loading?(
+        <p>...loading</p>
+      ):(
         <ul style={{listStyle:"none"}}>{
+    
           dates.map((date,index)=>(
-          (todaysDate.toString().substring(0,15)===date.toString().substring(0,15))?(
-          <li key={index} ref={this.myref}>  {">>>>>>>"+ date.toString()}</li>):(
-             <li key={index}> {date.toString()}</li>
-          )
+            
+           <Item key={ date.toString()} todaysDate={todaysDate} date={date} itemData={infoMap.get(date)}/>
             ))}
-        </ul>
+        </ul>)}
       </div>
       );
     }
--- src/components/item.component.jsx
+++ src/components/item.component.jsx
@@ -1,23 +1,47 @@
-import React from 'react';
+import React,{useRef, useEffect} from 'react';
 import {ListItem} from './item.styles.js';
 
-function Item({itemData:[date, info]}){
+
+ 
+function Item({date,todaysDate,itemData}){
+    const myRef= useRef(null);
+    useEffect(()=>{
+        if(myRef.current){
+        myRef.current.scrollIntoView({behavior:'smooth'});
+        }
+    })
     let options = { year: 'numeric', month: 'long', day: 'numeric' };
-
+    console.log(itemData[0]||null)
     return (
-        <ListItem>
+        (todaysDate.toString().substring(0,15)===date.toString().substring(0,15))?(
+        <div ref={myRef}>
+
             <h4>
-            {date.toLocaleDateString('en-US', options)}
-            </h4>
-
+            {"today>>>>>"+ date.toLocaleDateString('en-US', options)}
+            </h4>        
+        {itemData[0]?<p>{itemData[0][1]}</p>:null} 
             <hr style={{
-                color:'grey',
-                beackground:'grey',
-                height:'1px'
-            }}/>
+                   color:'grey',
+                   beackground:'grey',
+                   height:'1px'
+               }}/>
+            
+        </div>):(
+             <div>
 
-            {info?<p>{info.toString()}</p>:null}
-        </ListItem>
+             <h4>
+             {date.toLocaleDateString('en-US', options)}
+             </h4>
+ 
+            <br/>
+            {itemData[0]?<p>{itemData[0][1]}</p>:null}
+            <hr style={{
+                 color:'grey',
+                 beackground:'grey',
+                 height:'1px'
+             }}/>
+         </div>
+        )
     )
 }
 
